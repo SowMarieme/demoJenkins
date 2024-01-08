6a378d2633a8cd019151dc1eb447ab58e88f33e0
Marieme <sowmarieme@groupeisi.com>
2024-01-08 19:04:22 +0000

    firsts commits

---
+++ src/main/java/com/groupeisi/demoJenkits/DemoJenkitsApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.demoJenkits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/demoJenkits/DemoJenkitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.demoJenkits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
